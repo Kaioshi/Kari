e875a273af798591401288b5a4869dc15a5e6875
Kaioshi <waderr at gmail dot com>
2014-05-22 22:21:00 +0800

    [core] more commands~

--- plugins/core/core.go
+++ plugins/core/core.go
@@ -13,10 +13,9 @@ func Register(bot *irc.IRC) {
 		Handle: "autojoin",
 		Event:  "376",
 		Callback: func(input *events.Params) {
-			for _, channel := range bot.Config.Autojoin {
-				bot.Send("JOIN " + channel)
-			}
+			bot.Join(strings.Join(bot.Config.Autojoin, ","))
 		}})
+
 	// say
 	events.CmdListen(&events.CmdListener{
 		Command: "say",
@@ -25,6 +24,16 @@ func Register(bot *irc.IRC) {
 		Callback: func(input *events.Params) {
 			bot.Say(input.Context, strings.Join(input.Args, " "))
 		}})
+
+	// action
+	events.CmdListen(&events.CmdListener{
+		Command: "action",
+		Help:    "/me's stuff!",
+		Syntax:  bot.Config.Prefix + "action <thing you want the bot to emote>",
+		Callback: func(input *events.Params) {
+			bot.Action(input.Context, strings.Join(input.Args, " "))
+		}})
+
 	// join
 	events.CmdListen(&events.CmdListener{
 		Command: "join",
@@ -37,6 +46,19 @@ func Register(bot *irc.IRC) {
 				bot.Say(input.Context, "That ain't how you join a channel sucka")
 				return
 			}
-			bot.Send("JOIN " + input.Args[0])
+			bot.Join(input.Args[0])
+		}})
+
+	// part
+	events.CmdListen(&events.CmdListener{
+		Command: "part",
+		Help:    "Parts channels!",
+		Syntax:  bot.Config.Prefix + "part #channel",
+		Callback: func(input *events.Params) {
+			if len(input.Args) < 1 || input.Args[0][0:1] != "#" {
+				bot.Say(input.Context, "That ain't how you part a channel sucka")
+				return
+			}
+			bot.Part(input.Args[0])
 		}})
 }
