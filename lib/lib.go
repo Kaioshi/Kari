9305527501df729d1f1616fed27fb15fb43bf9c1
Kaioshi <waderr at gmail dot com>
2014-05-24 00:12:39 +0800

    [lib] add lib.StrList struct and helper functions - slices are stupiiidddd

--- lib/lib.go
+++ lib/lib.go
@@ -1,13 +1,75 @@
 package lib
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 	"time"
 )
 
-// helper functions
+// STRING LISTS ---- why isn't this built in or less pointlessly complex
+type StrList struct {
+	List []string
+}
 
+func (sl *StrList) Pop() {
+	if len(sl.List) == 0 {
+		return
+	}
+	sl.List = sl.List[:len(sl.List)-1]
+}
+
+func (sl *StrList) HasElement(match string, ignoreCase bool) bool {
+	if ignoreCase {
+		match = strings.ToLower(match)
+		for _, element := range sl.List {
+			if strings.ToLower(element) == match {
+				return true
+			}
+		}
+	} else {
+		for _, element := range sl.List {
+			if element == match {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (sl *StrList) Add(s ...string) {
+	sl.List = append(sl.List, s...)
+}
+
+func (sl *StrList) RemoveByMatch(match string, ignoreCase bool) {
+	if ignoreCase {
+		for i, value := range sl.List {
+			if strings.ToLower(value) == strings.ToLower(match) {
+				sl.RemoveByIndex(i)
+				break
+			}
+		}
+	} else {
+		for i, value := range sl.List {
+			if value == match {
+				sl.RemoveByIndex(i)
+				break
+			}
+		}
+	}
+}
+
+func (sl *StrList) RemoveByIndex(i int) {
+	s := sl.List
+	s = append(s[:i], s[i+1:]...)
+	sl.List = s
+}
+
+func (sl *StrList) String() string {
+	return fmt.Sprintf("%#v", sl.List)
+}
+
+// random helper functions
 func Sanitise(line string) string {
 	if !strings.ContainsAny(line, "\n\t\r") {
 		return line
