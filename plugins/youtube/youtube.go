2df39db502b4cf55e595a3ba242ac1af9c1c4e07
Kaioshi <waderr at gmail dot com>
2014-05-27 09:10:47 +0800

    [youtube] fondlings

--- plugins/youtube/youtube.go
+++ plugins/youtube/youtube.go
@@ -9,7 +9,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
-	"strings"
 	"time"
 )
 
@@ -22,10 +21,9 @@ type YouTubeFeed struct {
 }
 
 type YouTubeEntry struct {
-	Title  map[string]string `json:"title"`
-	Info   InfoEntry         `json:"media$group"`
-	Rating RatingEntry       `json:"gd$rating"`
-	Stats  map[string]string `json:"yt$statistics"`
+	Title map[string]*string `json:"title"`
+	Info  *InfoEntry         `json:"media$group"`
+	Stats map[string]string  `json:"yt$statistics"`
 }
 
 type InfoEntry struct {
@@ -33,12 +31,6 @@ type InfoEntry struct {
 	Duration    map[string]string `json:"yt$duration"`
 	Date        map[string]string `json:"yt$uploaded"`
 	ID          map[string]string `json:"yt$videoid"`
-}
-
-type RatingEntry struct {
-	Average float32 `json:"average"`
-	Max     int     `json:"max"`
-	Min     int     `json:"min"`
 }
 
 func Register(bot *irc.IRC) {
@@ -51,20 +43,27 @@ func Register(bot *irc.IRC) {
 		Callback: func(input *events.Params) {
 			ytr := &YouTubeResults{}
 			uri := fmt.Sprintf("https://gdata.youtube.com/feeds/api/videos?q=%s&max-results=1&v=2&alt=json",
-				url.QueryEscape(strings.Join(input.Args, " ")))
-			web.Get(uri, func(ERROR string, body []byte) {
-				err := json.Unmarshal(body, &ytr)
-				if err != nil {
-					logger.Error("Couldn't parse youtube's JSON:" + err.Error())
-					return
-				}
-				yt := &ytr.Feed.Entry[0]
-				duration, err := time.ParseDuration(yt.Info.Duration["seconds"] + "s")
-				resp := fmt.Sprintf("%s ~ [%s] %s - %s views ~ http://youtu.be/%s ~ %s",
-					yt.Title["$t"], duration, yt.Info.Date["$t"][0:10],
-					lib.CommaNum(yt.Stats["viewCount"]), yt.Info.ID["$t"],
-					yt.Info.Description["$t"])
-				bot.Say(input.Context, resp)
-			})
+				url.QueryEscape(input.Data))
+			body, err := web.Get(&uri)
+			if err != "" {
+				logger.Error("YouTube Failed: " + err)
+				bot.Say(input.Context, "Woops.")
+				return
+			}
+			errr := json.Unmarshal(body, &ytr)
+			if errr != nil {
+				logger.Error("Couldn't parse youtube's JSON:" + errr.Error())
+				bot.Say(input.Context, "Herp. Check logs")
+				return
+			}
+			yt := &ytr.Feed.Entry[0]
+			duration, errr := time.ParseDuration(yt.Info.Duration["seconds"] + "s")
+			resp := fmt.Sprintf("%s ~ [%s] %s - %s views ~ http://youtu.be/%s ~ %s",
+				*yt.Title["$t"], &duration, yt.Info.Date["$t"][0:10],
+				lib.CommaNum(yt.Stats["viewCount"]), yt.Info.ID["$t"],
+				yt.Info.Description["$t"])
+			bot.Say(input.Context, resp)
+			resp = ""
+			ytr = nil
 		}})
 }
